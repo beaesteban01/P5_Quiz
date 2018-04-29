33e3f257df0cd3f7cb8f69cb76fb6495da793a19
beaesteban01 <bea1596@gmail.com>
2018-04-29 19:46:03 +0200

    parte 1 intento 3
    credits con enlace

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/credits', (req, res, next)=> {
+	res.render('credits', {title: 'Credits'});
+});
+
 module.exports = router;
