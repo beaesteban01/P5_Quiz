d3809e18bc57d24794cc35bf0badb611f64a526c
beaesteban01 <bea1596@gmail.com>
2018-05-02 10:24:18 +0200

    version con quizzes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const Sequelize = require("sequelize");
-const {models} = require("../models");
+const Sequelize = require('sequelize');
+const {models} = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +16,7 @@ router.get('/credits', (req, res, next)=> {
 router.get('/quizzes', (req, res, next) => {
 	models.quiz.findAll()
 	.then(quizzes => {
-		res.render('quizzes', {title: 'quizzes'});
+		res.render('quizzes', {quizzes});
 	})
 	.catch(error => next(error));
 });
